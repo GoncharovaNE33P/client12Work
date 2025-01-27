0dc3297130f2ff483bdc2c907e858a823f54ef1e
GoncharovaNE <12bts.com@gmail.com>
2025-01-27 19:34:42 +0300

    осталось сделать оформление по стилю

---
+++ client/Models/PartialTours.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace client.Models
+{
+    public partial class Tour
+    {
+        public string ActualTour
+        {
+            get
+            {
+                if (IsActual == 0)
+                {
+                    return "Не актуален";
+                }
+                return "Актуален";
+            }
+        }
+    }
+}
